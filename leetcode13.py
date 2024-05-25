a59aa3f1b417d94ab64843c6650017e680eab24d
BenMenard <benjaminlmenard@gmail.com>
2024-05-25 15:04:58 -0400

    added leetcode13.py

---
+++ leetcode13.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        num = 0
+        
+        for i in range(len(s)):
+            if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i+1]]:
+                num -= roman_dict[s[i]]
+            else:
+                num += roman_dict[s[i]]
+        
+        return num
